016e17efb92b4806ec865bf4f367e56bda9f5f3f
Umberto Raimondi <me@uraimo.com>
2016-12-18 11:39:13 +0100

    Update checkoutRelease.sh

--- checkoutRelease.sh
+++ checkoutRelease.sh
@@ -2,8 +2,7 @@
 
 BRANCH=swift-3.0.2-RELEASE
 
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}'; git reset --hard HEAD" \;
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}'; git pull --tags" \;
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}'; git checkout $BRANCH" \;
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}'; patch -p1 < DIFFS/*.diff" \;
- 
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "echo Cleaning '{}';cd '{}'; git reset --hard HEAD" \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd Updating '{}'; git pull; git fetch --tags" \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd Switching '{}' to ${BRANCH}; git checkout ${BRANCH}" \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd Applying patches to '{}'; patch -p1 < DIFFS/*.diff" \;
