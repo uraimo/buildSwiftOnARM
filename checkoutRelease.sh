268aacc1ae800605b1837dd14afc5825494d9eb2
Umberto Raimondi <me@uraimo.com>
2016-12-30 16:54:14 +0100

    Moved diffs, fixed and improved script

--- checkoutRelease.sh
+++ checkoutRelease.sh
@@ -3,6 +3,6 @@
 BRANCH=swift-3.0.2-RELEASE
 
 find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "echo Cleaning '{}';cd '{}'; git reset --hard HEAD" \;
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd Updating '{}'; git pull; git fetch --tags" \;
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd Switching '{}' to ${BRANCH}; git checkout ${BRANCH}" \;
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd Applying patches to '{}'; patch -p1 < DIFFS/*.diff" \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "echo Updating '{}';cd '{}'; git pull; git fetch --tags" \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "echo Switching '{}' to ${BRANCH};cd '{}'; git checkout ${BRANCH}" \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "echo Applying patches to '{}';cd '{}'; patch -p1 < ../'{}'.diffs/*.diff" \;
