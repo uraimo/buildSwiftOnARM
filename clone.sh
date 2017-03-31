793ffbd5c4e7568faed8428e8e34ac3ef51b6023
Umberto Raimondi <me@uraimo.com>
2017-03-31 22:17:07 +0200

    Update clone.sh
    Clang 3.7 seems the safer option

--- clone.sh
+++ clone.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install git cmake ninja-build clang-3.9 python uuid-dev libicu-dev icu-devtools libbsd-dev libedit-dev libxml2-dev libsqlite3-dev swig libpython-dev libncurses5-dev pkg-config libblocksruntime-dev libcurl4-openssl-dev autoconf libtool systemtap-sdt-dev
+sudo apt-get install git cmake ninja-build clang-3.7 python uuid-dev libicu-dev icu-devtools libbsd-dev libedit-dev libxml2-dev libsqlite3-dev swig libpython-dev libncurses5-dev pkg-config libblocksruntime-dev libcurl4-openssl-dev autoconf libtool systemtap-sdt-dev
 
 git clone https://github.com/apple/swift
 
