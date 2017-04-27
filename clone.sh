a110cfaa36562c817001c974fbfe2101d20b681a
Umberto Raimondi <me@uraimo.com>
2017-04-27 16:02:21 +0000

    Select the correct Clang version for Ubuntu

--- clone.sh
+++ clone.sh
@@ -1,9 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo apt-get install git cmake ninja-build clang-3.7 python uuid-dev libicu-dev icu-devtools libbsd-dev libedit-dev libxml2-dev libsqlite3-dev swig libpython-dev libncurses5-dev pkg-config libblocksruntime-dev libcurl4-openssl-dev autoconf libtool systemtap-sdt-dev
+CLANG_VERSION=3.7
 
-sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.7 100
-sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.7 100
+## Use 3.8 on Ubuntu Mate, 3.7 everywhere else
+if [[ $((/usr/bin/lsb_release -a) 2>&1) =~ "Ubuntu" ]]; then
+  CLANG_VERSION=3.8
+fi
+
+
+sudo apt-get install git cmake ninja-build clang-$CLANG_VERSION python uuid-dev libicu-dev icu-devtools libbsd-dev libedit-dev libxml2-dev libsqlite3-dev swig libpython-dev libncurses5-dev pkg-config libblocksruntime-dev libcurl4-openssl-dev autoconf libtool systemtap-sdt-dev
+
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-$CLANG_VERSION 100
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-$CLANG_VERSION 100
 
 git clone https://github.com/apple/swift
 
