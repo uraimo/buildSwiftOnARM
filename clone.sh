d0b795e866326229139f1b790f6be03c583ab51f
Umberto Raimondi <me@uraimo.com>
2017-08-23 10:09:37 +0200

    Always use clang 3.8 when building
    Since the lastest release of Raspbian is based on Debian Strech, we can
    now just use clang 3.8.
    
    This new Raspbian release also makes 3.1.1 compilable.

--- clone.sh
+++ clone.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-CLANG_VERSION=3.7
-
-## Use 3.8 on Ubuntu Mate, 3.7 everywhere else
-if [[ $((/usr/bin/lsb_release -a) 2>&1) =~ "Ubuntu" ]]; then
-  CLANG_VERSION=3.8
-fi
-
+CLANG_VERSION=3.8
 
 sudo apt-get install git cmake ninja-build clang-$CLANG_VERSION python uuid-dev libicu-dev icu-devtools libbsd-dev libedit-dev libxml2-dev libsqlite3-dev swig libpython-dev libncurses5-dev pkg-config libblocksruntime-dev libcurl4-openssl-dev autoconf libtool systemtap-sdt-dev
 
