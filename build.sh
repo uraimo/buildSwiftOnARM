26b25c7120515f4baa9f3e705e66fd0170d42979
Jason Miller <jasonm@csail.mit.edu>
2018-05-08 17:27:16 -0400

    Add patches and support for 64-bit ARM (aarch64)

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ INSTALL_DIR=`pwd`/install
 PACKAGE=`pwd`/swift-${REL}.tgz
 #Determine architecture
 case `uname -m` in
+    aarch64)
+	ARCH=aarch64;;
     armv6*)
         ARCH=armv6;;
     *)
@@ -15,6 +17,8 @@ rm -rf $INSTALL_DIR $PACKAGE
 
 
 ./swift/utils/build-script --build-subdir buildbot_linux -R --lldb --llbuild --xctest --swiftpm --foundation --libdispatch -- --install-libdispatch --install-foundation --install-swift --install-lldb --install-llbuild --install-xctest --install-swiftpm --install-prefix=/usr '--swift-install-components=autolink-driver;compiler;clang-builtin-headers;stdlib;swift-remote-mirror;sdk-overlay;dev' --build-swift-static-stdlib --build-swift-static-sdk-overlay --install-destdir=${INSTALL_DIR} --installable-package=${PACKAGE}
+
+if ["x$ARCHx" != "xaarch64x"]; then
 
 echo "+ Fixing up the install package for ARM"
 cp -R swift-corelibs-libdispatch/dispatch/ ${INSTALL_DIR}/usr/lib/swift
@@ -32,3 +36,4 @@ cp swift-corelibs-libdispatch/os/object.h ${INSTALL_DIR}/usr/lib/swift/os
 echo "+ Retar installation package"
 tar -C ${INSTALL_DIR} -czf ${PACKAGE} .
 
+fi # !aarch64
