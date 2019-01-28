ac07e873b6ab096aed39fcee5a66cccc1c1c65c3
Umberto Raimondi <me@uraimo.com>
2019-01-28 10:32:55 +0000

    Add support for platform specific patches

--- checkoutRelease.sh
+++ checkoutRelease.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+. "$(dirname $0)/utils.sh"
 
 BRANCH=swift-4.2-branch
 TAG=swift-4.2.1-RELEASE
@@ -7,5 +8,7 @@ echo "♻️ \033[1m Resetting the repositories...\033[0m"
 find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "[ -d '{}'/.git ] && echo \\* Cleaning '{}' && cd '{}' && git reset --hard HEAD && git clean -fd" \;
 echo "✳️ \033[1m Switching all the repositories to ${BRANCH} @ ${TAG}...\033[0m"
 ./swift/utils/update-checkout --scheme ${BRANCH} --tag ${TAG}
-echo "✅ \033[1m Applying the required patches...\033[0m"
+echo "✅ \033[1m Applying the required cross-platform patches...\033[0m"
 find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "[ -d '{}'.diffs ] && echo \\* Applying patches to '{}' && cd '{}'  && for f in ../'{}'.diffs/*.diff; do patch -p1 < \$f; done;" \;
+echo "✅ \033[1m Applying the required $ARCH patches...\033[0m"
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "[ -d '{}'.diffs/$ARCH ] && echo \\* Applying patches to '{}' && cd '{}'  && for f in ../'{}'.diffs/$ARCH/*.diff; do patch -p1 < \$f; done;" \;
