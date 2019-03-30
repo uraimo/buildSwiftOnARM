6d3a06a3df181c13ad7ce8c290687a9fd956f5a8
Umberto Raimondi <me@uraimo.com>
2019-03-30 03:09:05 +0100

    Removed obsolete patches, added first new ones

--- checkoutRelease.sh
+++ checkoutRelease.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 . "$(dirname $0)/utils.sh"
 
-BRANCH=swift-4.2-branch
-TAG=swift-4.2.3-RELEASE
+BRANCH=swift-5.0-branch
+TAG=swift-5.0-RELEASE
 
 echo "♻️ \033[1m Resetting the repositories...\033[0m"
 find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "[ -d '{}'/.git ] && echo \\* Cleaning '{}' && cd '{}' && git reset --hard HEAD && git clean -fd" \;
