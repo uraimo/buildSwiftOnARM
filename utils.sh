279ef0f732f0548713cad492d6f5a30272d16521
Umberto Raimondi <me@uraimo.com>
2019-06-25 16:49:57 +0200

    [Scripts] Refactor OS,Version and Arch detection and add Buster

--- utils.sh
+++ utils.sh
@@ -1,19 +1,33 @@
 #!/bin/sh
 
-# Set PLATFORM and ARCH
-PLATFORM="unknown"
+# Set OS, VERSION and ARCH
+OS="unknown"
+VERSION="unknown"
+ARCH="unknown"
+
 if [[ $(cat /etc/os-release) = *"Ubuntu"* ]]; then
-  PLATFORM="ubuntu"
+    OS="ubuntu"
+    VERSION=`cat /etc/os-release | sed -n 's/VERSION_ID=\"\([0-9].*\)\"/\1/p'`
 elif [[ $(cat /etc/os-release) = *"Raspbian"* ]]; then
-  PLATFORM="raspbian"
+    OS="raspbian"
+    if [[ $(cat /etc/os-release) = *"strech"* ]]; then
+        VERSION="strech"
+    elif [[ $(cat /etc/os-release) = *"buster"* ]]; then
+        VERSION="buster"
+    fi
+elif [[ $(cat /etc/os-release) = *"strech"* ]]; then
+    OS="debian"
+    VERSION="strech"
+elif [[ $(cat /etc/os-release) = *"buster"* ]]; then
+    OS="debian"
+    VERSION="buster"
 fi
 
 case `uname -m` in
     aarch64)
-	ARCH=aarch64;;
+        ARCH=aarch64;;
     armv6*)
         ARCH=armv6;;
     *)
         ARCH=armv7;;
 esac
- 
