02409972c2407ef2428bc235dad02645500d2486
Umberto Raimondi <me@uraimo.com>
2019-06-25 17:01:29 +0200

    [Scripts] Add support for OS, ARCH, VERSION, OSVERSION patches
    Patches needed only for one specific variation of the above can now be
    placed in a subdirectory of <PROJECT>.diffs/ and will be applied only if
    needed.
    
    What are the supported values for now:
    
    ARCH: armv6, armv7, aarch64
    OS: debian, raspbian, ubuntu
    VERSION: stretch, buster, whatever version of ubuntu you got

--- checkoutRelease.sh
+++ checkoutRelease.sh
@@ -10,7 +10,12 @@ echo "✳️ \033[1m Switching all the repositories to ${BRANCH} @ ${TAG}...\033
 ./swift/utils/update-checkout --scheme ${BRANCH} --tag ${TAG}
 echo "✅ \033[1m Applying the required cross-platform patches...\033[0m"
 find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "[ -d '{}'.diffs ] && echo \\* Applying patches to '{}' && cd '{}'  && for f in ../'{}'.diffs/*.diff; do patch -p1 < \$f; done;" \;
-echo "✅ \033[1m Applying the required $ARCH patches...\033[0m"
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "[ -d '{}'.diffs/$ARCH ] && echo \\* Applying patches to '{}' && cd '{}'  && for f in ../'{}'.diffs/$ARCH/*.diff; do patch -p1 < \$f; done;" \;
-echo "✅ \033[1m Applying the required $PLATFORM patches...\033[0m"
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "[ -d '{}'.diffs/$PLATFORM ] && echo \\* Applying patches to '{}' && cd '{}'  && for f in ../'{}'.diffs/$PLATFORM/*.diff; do patch -p1 < \$f; done;" \;
+
+
+# Patches for a specific arch, OS, shared version(debian and raspbian can share patches) 
+# and OS version go in their own subdirectory
+for VARIANT in $ARCH $OS $VERSION $OS$VERSION
+do 
+    echo "✅ \033[1m Applying the required $VARIANT patches...\033[0m"
+    find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "[ -d '{}'.diffs/$VARIANT ] && echo \\* Applying patches to '{}' && cd '{}'  && for f in ../'{}'.diffs/$VARIANT/*.diff; do patch -p1 < \$f; done;" \;
+done
