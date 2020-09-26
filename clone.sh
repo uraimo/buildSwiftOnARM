623376867eec6e7d8d55c4e71f20bccbd8ba24ea
Umberto Raimondi <me@uraimo.com>
2020-09-26 18:10:01 +0200

    Add dependency: python-six for python2.7

--- clone.sh
+++ clone.sh
@@ -8,7 +8,7 @@ fi
 
 # Basic dependencies
 echo "✅ Installing dependencies..."
-sudo apt install -y make patch rsync git cmake ninja-build clang$CLANG_VERSION python uuid-dev libicu-dev icu-devtools libbsd-dev libedit-dev libxml2-dev libsqlite3-dev swig libpython-dev libncurses5-dev pkg-config libblocksruntime-dev libcurl4-openssl-dev autoconf libtool systemtap-sdt-dev libcurl4-openssl-dev libz-dev
+sudo apt install -y make patch rsync git cmake ninja-build clang$CLANG_VERSION python uuid-dev libicu-dev icu-devtools libbsd-dev libedit-dev libxml2-dev libsqlite3-dev swig libpython-dev libncurses5-dev pkg-config libblocksruntime-dev libcurl4-openssl-dev autoconf libtool systemtap-sdt-dev libcurl4-openssl-dev libz-dev python-six vim
 
 # Some OS or specific versions could have additional requirements, if they do
 # a script will be present in distro-scripts and we'll run it right after the more generic configuration
